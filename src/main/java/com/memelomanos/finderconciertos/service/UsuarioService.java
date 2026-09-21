bc686a92bdad125ae27f831b5c9c6f901ca48d83
saulss <saul_ss@ciencias.unam.mx>
2026-09-20 19:26:39 -0600

    Implementacion del endpoint de favoritos

--- src/main/java/com/memelomanos/finderconciertos/service/UsuarioService.java
+++ src/main/java/com/memelomanos/finderconciertos/service/UsuarioService.java
@@ -2,7 +2,9 @@ package com.memelomanos.finderconciertos.service;
 
 import com.memelomanos.finderconciertos.exception.CorreoDuplicadoException;
 import com.memelomanos.finderconciertos.exception.UsuarioNoEncontradoException;
+import com.memelomanos.finderconciertos.model.Concierto;
 import com.memelomanos.finderconciertos.model.Usuario;
+import com.memelomanos.finderconciertos.repository.ConciertoRepository;
 import com.memelomanos.finderconciertos.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,13 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final ConciertoRepository conciertoRepository;
 
     @Autowired
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository,
+                           ConciertoRepository conciertoRepository) {
         this.usuarioRepository = usuarioRepository;
+        this.conciertoRepository = conciertoRepository;
     }
 
     public Usuario registrarUsuario(String nombre, String correo) {
@@ -31,5 +36,17 @@ public class UsuarioService {
     public Usuario buscarPerfilPorCorreo(String correo) {
         return usuarioRepository.findByCorreo(correo)
                 .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+    }
+
+    public Usuario agregarFavorito(Long usuarioId, Long conciertoId) {
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+        Concierto concierto = conciertoRepository.findById(conciertoId)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Concierto no encontrado"));
+
+        usuario.agregarFavorito(concierto);
+
+        return usuarioRepository.save(usuario);
     }
 }
